d5a0c61f43a14a722f41749bb2e73c79fa1093a7
Vlad Bond <foolowin@gmail.com>
2020-11-03 18:07:39 +0300

    start

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabcontainer');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('active')
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('active')
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.matches('.tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    })
+
+
+
+
+
+})
