b434cc5a16c26b90f362c047e78153684825426b
Vlad Bond <foolowin@gmail.com>
2020-11-07 20:14:04 +0300

    ajax fetch async await

--- src/js/main.js
+++ src/js/main.js
@@ -186,44 +186,56 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new MenuCard(
-        `"img/tabs/vegy.jpg"`,
-        `"vegy"`,
-        `Меню "Фитнес"`,
-        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
-        овощей и фруктов. Продукт активных и здоровых людей. 
-        Это абсолютно новый продукт с оптимальной
-        ценой и высоким качеством!`,
-        9,
-        '.menu__field .container',
-        'big'
-    ).render();
+    const getResource = async (url) => {
+        // Async-await, синхронизирует присвоение result, перед тем как его вернет функция
+        // вплоть до 30 секунд
+        const result = await fetch(url);
 
-    new MenuCard(
-        `"img/tabs/elite.jpg"`,
-        `"elite"`,
-        `Меню “Премиум”`,
-        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но
-        и качественное исполнение блюд. Красная рыба,
-         морепродукты, фрукты - ресторанное меню без похода
-        в ресторан!`,
-        14,
-        '.menu__field .container',
-        'menu__item'
-    ).render();
+        // Обработка поведения фетча при ошибках HTTP протокола
+        // 2 свойства промиса возврощающегося от фетч .ok status
+        // С результатом чтото не Ок=))
+        if (!result.ok) {
+            // Объект ошибки
+            throw new Error(`Could not fetch ${url}, status ${result.status}`);
+        }
 
-    new MenuCard(
-        `"img/tabs/post.jpg"`,
-        `"post"`,
-        `Меню "Постное"`,
-        `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие
-        продуктов животного происхождения, молоко из миндаля,
-         овса, кокоса или гречки, правильное
-        количество белков за счет тофу и импортных вегетарианских стейков. `,
-        21,
-        '.menu__field .container',
-        'menu__item'
-    ).render();
+        return await result.json();
+
+    };
+
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({ img, altimg, title, descr, price }) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+
+    // function createCard(data) {
+    //     data.forEach(({ img, altimg, title, descr, price }) => {
+    //         const element = document.createElement('div');
+
+    //         price *= 27;
+
+    //         element.classList.add('menu__item');
+
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //             </div>
+    //         `;
+    //         document.querySelector('.menu .container').append(element);
+
+    //     });
+
+    // }
 
     // AJAX
 
@@ -236,10 +248,25 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        // Async-await, синхронизирует присвоение result, перед тем как его вернет функция
+        // вплоть до 30 секунд
+        const result = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await result.json();
+
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -262,7 +289,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 obj[key] = value;
             });
 
-            // const json = JSON.stringify(obj);
+            // Новый стандарт приеобразования FormData в JSON
+
+            // const json = JSON.stringify(Object.fromEntries(FormData.entries()));
+
+            const json = JSON.stringify(obj);
 
             // Fetch POST через FormData
             // Promise который запускается при помощи fetch`a не перейдет в состаяние
@@ -270,18 +301,19 @@ document.addEventListener('DOMContentLoaded', () => {
             // он всеравно выполнится нормально меняется только свойство status 
             // которое перейдет в значение false, reject будет возницать,
             // только при сбое сети или падении сервера.
-            fetch('server.php', {
-                method: "POST",
-                // headers: {
-                //     'Content-type': 'application/json'
-                // },
-                // Отправляем данные в формате FormData
-                // body: formData
-                // Отправляем данные в формате JSON
-                body: JSON.stringify(obj)
-                // Data данные которые вернет сервер после поста
-                // преобразовываем ответ в текст методом фетча
-            }).then(data => data.text())
+            // fetch('server.php', {
+            //     method: "POST",
+            // headers: {
+            //     'Content-type': 'application/json'
+            // },
+            // Отправляем данные в формате FormData
+            // body: formData
+            // Отправляем данные в формате JSON
+            // body: JSON.stringify(obj)
+            // Data данные которые вернет сервер после поста
+            // преобразовываем ответ в текст методом фетча
+            // })
+            postData('http://localhost:3000/requests', json)
                 .then(data => {
                     console.log(data);
                     showThanksModal(message.success);
